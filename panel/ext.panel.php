b4c58231a62dcbc1e654abb243e6a869fab2b556
Adam Fairholm <adam.fairholm@gmail.com>
2010-12-16 14:55:15 -0500

    Added the extension that adds in the global vars.

---
+++ panel/ext.panel.php
@@ -0,0 +1,85 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Panel_ext {
+
+    var $name 				= 'Panel Extenson';
+    var $version 			= '0.1';
+    var $description 		= 'Adds panel settings into global variables';
+    var $settings_exist 	= 'n';
+    var $docs_url 			= '';
+
+	// --------------------------------------------------------------------------
+
+	/**
+	 * Constructor
+ 	 *
+ 	 * @access	public
+ 	 * @return 	void
+     */
+	function Panel_ext()
+	{
+		$this->EE =& get_instance();
+	}
+
+	// --------------------------------------------------------------------------
+
+    /**
+     * Set the panel vars.
+ 	 *
+ 	 * @access	public
+ 	 * @return 	void
+     */
+    function set_panel_vars()
+    {    
+    	$obj = $this->EE->db->get('panel_settings');
+    
+    	$settings = $obj->result();
+    	
+    	foreach( $settings as $setting ):
+    
+			$this->EE->config->_global_vars[$setting->setting_name] = $setting->value;
+		
+		endforeach;
+    }
+
+	// --------------------------------------------------------------------------
+	  
+	/**
+	 * Activate Extension
+	 *
+ 	 * @access	public
+ 	 * @return 	void
+     */
+	function activate_extension()
+	{
+		$data = array(
+			'class'		=> __CLASS__,
+			'method'	=> 'set_panel_vars',
+			'hook'		=> 'sessions_start',
+			'settings'	=> '',
+			'priority'	=> 8,
+			'version'	=> $this->version,
+			'enabled'	=> 'y'
+		);
+		
+		$this->EE->db->insert('extensions', $data);
+	}
+
+	// --------------------------------------------------------------------------
+
+	/**
+	 * Disable Extension
+	 *
+ 	 * @access	public
+ 	 * @return 	void
+     */
+	function disable_extension()
+	{
+		$this->EE->db->where('class', __CLASS__);
+		$this->EE->db->delete('extensions');
+	}
+
+}
+
+/* End of file ext.panel.php */
+/* Location: ./expressionengine/third_party/Panel/panel/ext.panel.php */
