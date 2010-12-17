e4a167c4cde2df6f25f95f120aae72d7e28eb904
Adam Fairholm <adam.fairholm@gmail.com>
2010-12-16 19:35:33 -0500

    Textarea setting.

---
+++ panel/settings/textarea/language/english/lang.textarea.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['textarea'] = array(
+
+'setting_label'					=> "Textarea"
+
+);
+
+/* End of file lang.textarea.php */
+/* Location: ./system/expressionengine/panel/settings/textarea/language/english/lang.textarea.php */
---
+++ panel/settings/textarea/setting.textarea.php
@@ -0,0 +1,32 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * Textarea Setting
+ *
+ * @package		Panel
+ * @author		Adam Fairholm (Green Egg Media)
+ */
+class Setting_textarea
+{
+	var $setting_type_name			= 'textarea';
+
+	/**
+	 * Output form input
+	 *
+	 * @param	array
+	 * @param	array
+	 * @return	string
+	 */
+	function form_output( $name, $value = '' )
+	{
+		$params['name']		= $name;
+		$params['id']		= $name;
+		$params['value']	= $value;
+		
+		return form_textarea( $params );
+	}
+	
+}
+
+/* End of file setting.textarea.php */
+/* Location: ./expressionengine/third_party/panel/settings/textarea/setting.textarea.php */
