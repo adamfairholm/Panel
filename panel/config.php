9fb1b21e4b9eeb727a3166fe10ca8251f77762e1
Adam Fairholm <adam.fairholm@gmail.com>
2011-10-31 17:52:50 -0400

    Adding NSM support.

---
+++ panel/config.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * NSM Add-on Updater Integration
+ */
+
+$config['name']									= 'Panel';
+$config['version']								= '1.0.2';
+$config['nsm_addon_updater']['versions_xml']	= 'http://parse19.com/panel/releases';
